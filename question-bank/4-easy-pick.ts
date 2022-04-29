1396d3f88101b7e326db48bd441c1794e68afd85
TTiip <2711430287@qq.com>
2022-04-29 11:11:49 +0800

    feat: 4-easy-pick

---
+++ question-bank/4-easy-pick.ts
@@ -0,0 +1,44 @@
+import type {
+  Equal, Expect
+} from '@type-challenges/utils'
+
+interface Todo {
+  title: string
+  description: string
+  completed: boolean
+}
+
+interface Expected1 {
+  title: string
+}
+
+interface Expected2 {
+  title: string
+  completed: boolean
+}
+
+// 1.返回一个对象
+// 2.遍历map
+// 3.判断key是否在todoList中
+// 4.如果在，则添加到picked中
+
+// const MyPick = (todoList, keys) => {
+//   const picked = {}
+//   keys.map(key => {
+//     if (key in todoList) {
+//       picked[key] = todoList[key]
+//     }
+//   })
+//   return picked
+// }
+
+type MyPick<T, K extends keyof T> = {
+	[key in K]: T[key]
+}
+
+type cases = [
+  Expect<Equal<Expected1, MyPick<Todo, 'title'>>>,
+  Expect<Equal<Expected2, MyPick<Todo, 'title' | 'completed'>>>,
+  // @ts-expect-error
+  MyPick<Todo, 'title' | 'completed' | 'invalid'>
+]
