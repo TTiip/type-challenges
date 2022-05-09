00200100762b3228b300878bd85e7f9de2175043
TTiip <2711430287@qq.com>
2022-05-09 14:22:39 +0800

    feat: 00014-easy-first 四种类型写法。

--- question-bank/00014-easy-first.ts
+++ question-bank/00014-easy-first.ts
@@ -17,29 +17,32 @@ function fitst (arr) {
   return first
 }
 
-// 写法一：
+// 写法一：===============================================================
 // 'T extends [] ? never: T[0]'
 // 这里的 'T extends []' 的意思是 判断T是不是一个空数组
 
 // type First<T extends any[]> = T extends [] ? never: T[0]
 
-// 写法二：
+// 写法二：===============================================================
 // 'T['length'] extends 0 ? never: T[0]'
 // 这里的 'T['length'] extends 0' 的意思是 判断T的长度是不是0，从而判断T是不是一个空数组
+
 // type First<T extends any[]> = T['length'] extends 0 ? never: T[0]
 
-// 写法三：
+// 写法三：===============================================================
 // 'T[0] extends T[number] ? T[0]: never'
 // 这里的 'T[0] extends T[number]' 的意思是 判断T的第一个元素在不在T中
 // 这里的 'T[0]' 的意思是 T的第一个元素 数组为空时 返回 undefined
 // 这里的 'T[number]' 的意思是 T 元素的 union 类型 [1, 2, 3] ---> 1|2|3
+
 // type First<T extends any[]> = T[0] extends T[number] ? T[0]: never
 
-// 写法四：
+// 写法四：===============================================================
 // 'T extends [infer First, ...infer Rest] ? First : never'
 // 这里的 'T extends [infer First, ...infer Rest]' 的意思是 使用 infer 关键字 将数组中的变量自动推断成对应的变量
 // [1, 2, 3, 4, 5] ---->  [infer aa, infer bb, infer cc,...infer Rest] ---> aa = 1, bb = 2, cc = 3, Rest = [4, 5]
 // T extends [infer First, ...infer Rest] ? First : never --> 如果 First 存在 则直接返回 First 否则返回 never
+
 type First<T extends any[]> = T extends [infer First, ...infer Rest] ? First : never
 
 type t1 = First<[[4, 5], 1, 2, 3 ]>
